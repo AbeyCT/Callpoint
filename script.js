17514b382d364df503538e371196d272b2c40a2d
AbeyTech <adejumobia3@gmail.com>
2024-08-27 18:10:56 +0100

    organised

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navItems = document.querySelector(".nav-items");
+
+hamburger.addEventListener("click", () => {
+  navItems.classList.toggle("active");
+  hamburger.classList.toggle("active"); // Toggle the "X" effect
+});
